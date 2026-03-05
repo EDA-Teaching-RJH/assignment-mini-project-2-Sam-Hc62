a19097fc3aafcfaf57d1bd8fa4d82c7e8a2563a5
Sam-Hc62 <sam.hcummins@gmail.com>
2026-03-05 11:30:14 +0000

    defining constants

--- performance_calc.py
+++ performance_calc.py
@@ -1,3 +1,10 @@
 import math #for the calculation 
 import re #for accepting correct inputs
 import json #for saving and loading
+def main():
+    air_density = 1.225   #defining constants
+    gravity = 9.81
+    rolling_resistance_coefficient = 0.015
+    target_speed = 26.8  #26m/s=60mph
+    time_step = 0.01
+
