3531659b53298c65d873070bd58a1f6365536cdd
Sam-Hc62 <sam.hcummins@gmail.com>
2026-03-05 17:44:18 +0000

    comments added

--- performance_calc.py
+++ performance_calc.py
@@ -20,7 +20,7 @@ def main():
         velocity = 0.00000001 #negligable start velocity to not divide by 0
         time = 0
         while velocity < target_speed: #count until 60mph is reached
-            engine_force = (Car.power*Car.efficiency)/velocity
+            engine_force = (Car.power*Car.efficiency)/velocity 
             drag_force = 0.5*air_density*Car.drag*Car.area*velocity**2
             rolling_force = rolling_resistance_coefficient*Car.mass*gravity
             net_force = engine_force-drag_force-rolling_force
@@ -36,9 +36,9 @@ def main():
 
             if not re.fullmatch(r"^\d{3,4}$", mass_value): #only allows 3-4 digits
                 print("Must be a realistic value...")
-                continue
+                continue #asks for input again
             else:
-                return float(mass_value) #converting string to float
+                return float(mass_value) #converting string to float and returning the value to the menu and then class
             
     def get_power(): #function to get input for engine power
         while True:
@@ -46,9 +46,9 @@ def main():
 
             if not re.fullmatch(r"^\d{5,7}$", power_value): #only accepts number between 5 and 7 digits long
                 print('Must be a realistic value...')
-                continue
+                continue #asks for input again
             else:
-                return float(power_value) #converting string to float
+                return float(power_value) #converting string to float and returning the value to the menu and then class
             
     def get_drag(): #function to get drag input
         while True:
@@ -56,9 +56,9 @@ def main():
 
             if not re.fullmatch(r"^0\.\d{2}$", drag_value): #only accepting 0.XY format
                 print('Must be a realistic value...')
-                continue
+                continue #asks for input again
             else:
-                return float(drag_value) #converting string to float
+                return float(drag_value) #converting string to float and returning the value to the menu and then class
             
     def get_area(): #function to get area input
         while True:
@@ -66,9 +66,9 @@ def main():
 
             if not re.fullmatch(r"^(?:1\.[5-9]\d*|[23](\.\d+)?)$", area_value): #only accepting numbers between 1.5 and 4
                 print('Must be a realistic value...')
-                continue
+                continue #asks for input again
             else:
-                return float(area_value) #converting string to float
+                return float(area_value) #converting string to float and returning the value to the menu and then class
             
     def get_efficiency(): #function to get efficiency input
         while True:
@@ -76,9 +76,9 @@ def main():
 
             if not re.fullmatch(r"^0\.\d{2}$", efficiency_value): #only accepting 0.XY format
                 print('Must be a realistic value...')
-                continue
+                continue #asks for input again
             else:
-                return float(efficiency_value) #converting string to float
+                return float(efficiency_value) #converting string to float and returning the value to the menu and then class
 
 
     def display_values(cls, Car): #function to displays current values in a table format
@@ -91,8 +91,9 @@ def main():
         car = Car(1600,120000,0.25,2.2,0.85) #estimated averages in place already if any info is missing calculations can still be done
 
         while True:
+            #printing menu options for user
             print('\n(1) Calculate 0-60mph\n(2) Change mass\n(3) Change engine power\n(4) Change drag coefficient\n(5) Change frontal area\n(6) Change drivetrain efficiency\n(7) Save car\n(8) Load car\n(10) Display values\n (11) Exit')
-            choice = input('Choose an option: ')
+            choice = input('Choose an option: ') #getting user input for menu choice
 
             if not choice.isdigit(): #make sure input is a number
                 continue
@@ -100,19 +101,23 @@ def main():
             choice = int(choice) #converting string to integer
 
             if choice == 1: 
-                time = calculate_time(Car) 
-                print(f'0-60mph in: {time} seconds') 
+                time = calculate_time(Car) #opens the calculation function 
+                print(f'0-60mph in: {time} seconds')  #prints the result of the calculation
             elif choice == 2:
-                Car.mass = get_mass()
-                print(f'Mass changed to {Car.mass}')
+                Car.mass = get_mass() #opens the function to get mass input and changes the value in the class
+                print(f'Mass changed to {Car.mass}') #prints the new value of mass
             elif choice ==3:
-                Car.power = get_power()
+                Car.power = get_power() #opens the function to get engine power input and changes the value in the class
+                print(f'Engine power changed to {Car.power}') #prints the new value of engine power
             elif choice == 4:
-                Car.drag = get_drag()
+                Car.drag = get_drag() #opens the function to get drag coefficient input and changes the value in the class
+                print(f'Drag coefficient changed to {Car.drag}') #prints the new value of drag coefficient
             elif choice == 5:
-                Car.area = get_area()
+                Car.area = get_area() #opens the function to get frontal area input and changes the value in the class
+                print(f'Frontal area changed to {Car.area}') #prints the new value of frontal area
             elif choice == 6:
-                Car.efficiency = get_efficiency()
+                Car.efficiency = get_efficiency() #opens the function to get drivetrain efficiency input and changes the value in the class
+                print(f'Drivetrain efficiency changed to {Car.efficiency}') #prints the new value of drivetrain efficiency
             elif choice == 10:
                 display_values()
     menu()
