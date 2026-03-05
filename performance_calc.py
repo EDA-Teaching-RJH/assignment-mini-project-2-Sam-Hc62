201e8650ad56df3d0f326ace52ca6966feb39fe3
Sam-Hc62 <sam.hcummins@gmail.com>
2026-03-05 11:38:51 +0000

    class for inputs

--- performance_calc.py
+++ performance_calc.py
@@ -2,9 +2,20 @@ import math #for the calculation
 import re #for accepting correct inputs
 import json #for saving and loading
 def main():
-    air_density = 1.225   #defining constants
+    #defining constants
+    air_density = 1.225   
     gravity = 9.81
     rolling_resistance_coefficient = 0.015
-    target_speed = 26.8  #26m/s=60mph
+    target_speed = 26.8  # 26m/s = 60mph
     time_step = 0.01
+    
+    class car: #setting class for values to be input
+        def __init__(self,mass,power,drag,area,efficiency):
+            self.mass = mass
+            self.power = power
+            self.drag = drag
+            self.area = area
+            self.efficiency = efficiency
+
+
 
