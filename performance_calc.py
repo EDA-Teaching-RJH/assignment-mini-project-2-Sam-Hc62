9cc7e9e411b582a04eafeb4734ba074c0999381f
Sam-Hc62 <sam.hcummins@gmail.com>
2026-03-05 23:30:12 +0000

    debugged minor errors

--- performance_calc.py
+++ performance_calc.py
@@ -35,50 +35,58 @@ def main():
             mass_value = input('\nMass (Kg): ')
 
             if not re.fullmatch(r"^\d{3,4}$", mass_value): #only allows 3-4 digits
-                print("Must be a realistic value...")
+                print("Must be a 3-4 digit integer...") 
                 continue #asks for input again
             else:
-                return float(mass_value) #converting string to float and returning the value to the menu and then class
+                return int(mass_value) #converting string to integer and returning the value to the menu and then class
             
     def get_power(): #function to get input for engine power
         while True:
             power_value = input('\nEngine Power (kW): ')
 
             if not re.fullmatch(r"^\d{2,4}$", power_value): #only accepts number between 2 and 4 digits long
-                print('Must be a realistic value...')
+                print('Must be a 2-4 digit integer...')
                 continue #asks for input again
             else:
                 return float(power_value)*1000 #converting string to float and returning the value to the menu and then class, x1000 to convert from kW to W
             
     def get_drag(): #function to get drag input
         while True:
-            drag_value = input('\nDrag Coefficient: ')
+            drag_value = input('\nDrag Coefficient (%): ')
 
-            if not re.fullmatch(r"^0\.\d{2}$", drag_value): #only accepting 0.XY format
-                print('Must be a realistic value...')
+            if not re.fullmatch(r"^\d{1,2}$", drag_value): #only accepting 1-2 digit numbers
+                print('Must be a one or two digit integer...')
                 continue #asks for input again
             else:
-                return float(drag_value) #converting string to float and returning the value to the menu and then class
+                return float(drag_value)/100 #converting string to float and returning the value to the menu and then class
             
     def get_area(): #function to get area input
         while True:
             area_value = input('\nFrontal Area (m²): ')
 
             if not re.fullmatch(r"^(?:1\.[5-9]\d*|[23](\.\d+)?)$", area_value): #only accepting numbers between 1.5 and 4
-                print('Must be a realistic value...')
-                continue #asks for input again
+                if not area_value.isdigit():
+                    print('Error...')
+                    continue
+                area_value = float(area_value) #converting string to float to check the value
+                if area_value > 4:
+                    print('Must be less than 4m²')
+                    continue
+                elif area_value < 1.5:
+                    print('Must be greater than 1.5m²')
+                    continue
             else:
                 return float(area_value) #converting string to float and returning the value to the menu and then class
             
     def get_efficiency(): #function to get efficiency input
         while True:
-            efficiency_value = input('\nDrivetrain Efficiency: ')
+            efficiency_value = input('\nDrivetrain Efficiency (%): ')
 
-            if not re.fullmatch(r"^0\.\d{2}$", efficiency_value): #only accepting 0.XY format
-                print('Must be a realistic value...')
+            if not re.fullmatch(r"^\d{2}$", efficiency_value): #only accepting 2 digit numbers
+                print('Must be a two digit integer...')
                 continue #asks for input again
             else:
-                return float(efficiency_value) #converting string to float and returning the value to the menu and then class
+                return float(efficiency_value)/100 #converting string to float and returning the value to the menu and then class
 
     def save_to_file(car): #function to save values to a json file
         try:
@@ -86,9 +94,14 @@ def main():
                 cars = json.load(f) 
         except (FileNotFoundError, json.JSONDecodeError): #if file does not exist or is empty, start with an empty list
             cars = []
-
-        name = input("Enter a name for this car: ").strip() #asking user for a name to save the car under, stripping whitespace from the input
-        cars.append({'name': name,'mass': car.mass,'power': car.power,'drag': car.drag,'area': car.area,'efficiency': car.efficiency}) #adding the new car to the list of cars 
+        while True:
+            name = input("Enter a name for this car: ").strip() #asking user for a name to save the car under, stripping whitespace from the input
+            if len(name)>=20:
+                print("Name must be less than 20 characters...\n")
+                continue #asks for input again
+            else:
+                break
+        cars.append({'name': name,'mass': int(car.mass),'power': int(car.power/1000),'drag': round(car.drag, 2),'area': car.area,'efficiency': car.efficiency}) #adding the new car to the list of cars 
 
         try:
             with open("car_data.json", "w") as f: #opening the file in write mode and saving the updated list of cars back to the file
@@ -111,31 +124,32 @@ def main():
             return None
 
         print("\nSaved Cars:")
-        print("-"*70)
-        print(f"{'No.':<4}{'Name':<15}{'Mass':<10}{'Power':<10}{'Drag':<10}{'Area':<10}{'Efficiency'}")
-        print("-"*70)
+        print("-"*82)
+        print(f"|{' No.':<4}{'| Name':<20}{'| Mass(Kg)':<11}{'| Power(kW)':<12}{'| Drag':<8}{'| Area(m²)':<11}| {'Efficiency'}  |")
+        print("-"*82)
         for idx, c in enumerate(cars, 1):
-            print(f"{idx:<4}{c['name']:<15}{c['mass']:<10}{c['power']:<10}{c['drag']:<10}{c['area']:<10}{c['efficiency']*100}%") #printing the list of saved cars in a table format with an index number for selection
-        print("-"*70)
+            print(f"| {idx:<3}| {c['name']:<18}| {c['mass']:<9}| {c['power']:<10}| {c['drag']:<6}| {c['area']:<9}| {int(c['efficiency']*100)}%         |") #printing the list of saved cars in a table format with an index number for selection
+        print("-"*82)
 
         while True:
             try:
-                choice_2 = int(input("Enter the number of the car to load: "))
+                choice_2 = int(input("\nEnter the number of the car to load: "))
                 if 1 <= choice_2 <= len(cars):
                     selected = cars[choice_2-1]
-                    return Car(mass=selected['mass'],power=selected['power'],drag=selected['drag'],area=selected['area'],efficiency=selected['efficiency'])
+                    return Car(mass=selected['mass'],power=selected['power']*1000,drag=selected['drag'],area=selected['area'],efficiency=selected['efficiency'])
                 else:
-                    print("Choice out of range. Try again.")
+                    print("enter a valid number from the list...")
+                    continue
             except ValueError:
-                print("Please enter a valid number.")
+                continue
             
     def display_values(car): #function to displays current values in a table format
         print("\nCurrent values:")
-        print("-"*81)
-        print(f"{'Mass(Kg)':<10}{'Engine Power(W)':<20}{'Drag Coefficient':<20}{'Frontal Area(m²)':<20}{'Efficiency':<11}")
-        print("-"*81)
-        print(f"{car.mass:<10}{car.power:<20}{car.drag:<20}{car.area:<20}{car.efficiency*100}%")
-        print("-"*81)
+        print("-"*76)
+        print(f"| {'Mass':<8}| {'Engine Power':<14}| {'Drag Coefficient':<18}| {'Frontal Area':<14}| {'Efficiency':<9} |")
+        print("-"*76)
+        print(f"{f'| {car.mass}Kg':<10}{f'| {int(car.power/1000)}kW':<16}{f'| {car.drag}':<20}{f'| {car.area}m²':<16}{f'| {int(car.efficiency*100)}%':<13}|")
+        print("-"*76)
     def menu(): #display menu for user to chose what action to take
         car = Car(1600,120000,0.25,2.2,0.85) #estimated averages in place already if any info is missing calculations can still be done
 
@@ -151,13 +165,13 @@ def main():
 
             if choice == 1: 
                 time = calculate_time(car) #opens the calculation function 
-                print(f'0-60mph in: {time} seconds')  #prints the result of the calculation
+                print(f'\n0-60mph in: {time}s')  #prints the result of the calculation
             elif choice == 2:
                 car.mass = get_mass() #opens the function to get mass input and changes the value in the class
-                print(f'Mass changed to {car.mass}Kg') #prints the new value of mass
+                print(f'Mass changed to {int(car.mass)}Kg') #prints the new value of mass
             elif choice ==3:
                 car.power = get_power() #opens the function to get engine power input and changes the value in the class
-                print(f'Engine power changed to {car.power}W') #prints the new value of engine power
+                print(f'Engine power changed to {int(car.power/1000)}kW') #prints the new value of engine power
             elif choice == 4:
                 car.drag = get_drag() #opens the function to get drag coefficient input and changes the value in the class
                 print(f'Drag coefficient changed to {car.drag}') #prints the new value of drag coefficient
@@ -166,7 +180,7 @@ def main():
                 print(f'Frontal area changed to {car.area}m²') #prints the new value of frontal area
             elif choice == 6:
                 car.efficiency = get_efficiency() #opens the function to get drivetrain efficiency input and changes the value in the class
-                print(f'Drivetrain efficiency changed to {car.efficiency}') #prints the new value of drivetrain efficiency
+                print(f'Drivetrain efficiency changed to {car.efficiency*100}%') #prints the new value of drivetrain efficiency
             elif choice == 7:
                 save_to_file(car) #opens the function to save the values to a json file
             elif choice == 8:
@@ -179,6 +193,6 @@ def main():
                 print("Exiting program...")
                 break #exits the loop and ends the program
             else:
-                print("Invalid choice. Please try again.") #if the user input does not match any of the options, it will print an error message and ask for input again
+                continue #if the user input does not match any of the options, it will ask for input again
     menu()
 main()
